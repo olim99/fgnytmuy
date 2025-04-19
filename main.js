ea724dd66fd2c59fcb6852c6771dd3a38ba75cfa
olim99 <olimchik074@gmail.com>
2025-04-19 15:53:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// import Typed from "typed.js";
+
+    let typed = new Typed("#heading", {
+      strings: ["Netflix", "Splash Screen"],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    });
